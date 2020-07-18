10154f3c2c2fcef2d8c27f6faf4e667c0f3f4ed6
Dennis Amparo <dennisamparo11@gmail.com>
2020-07-18 13:35:34 -0700

    new layout

---
+++ script.js
@@ -0,0 +1,60 @@
+// $(document).ready(function(){
+//     $('.collapsible').collapsible();
+//   });
+
+// var elem = document.querySelector('.collapsible.expandable');
+// var instance = M.Collapsible.init(elem, {
+//   accordion: false
+// });
+
+// function displayblue () 
+// { smallTimer = setInterval(function(){$("#statement").show()}, 2000)
+// };
+// displayblue();
+// clearInterval();
+
+
+
+
+
+$(".circleone").on("click", function (event) {
+  event.preventDefault();
+  console.log("hello");
+  $("#statement").show();
+  $("#about").hide();
+  $("#myworks").hide();
+  $("#contactme").hide();
+  $("#socialmedia").show();
+});
+
+$(".circletwo").on("click", function (event) {
+  event.preventDefault();
+  console.log("hello");
+  $("#about").show();
+  $("#statement").hide();
+  $("#myworks").hide();
+  $("#contactme").hide();
+  $("#socialmedia").show();
+});
+
+$(".circlethree").on("click", function (event) {
+  event.preventDefault();
+  console.log("hello");
+  $("#myworks").show();
+  $("#about").hide();
+  $("#statement").hide();
+  $("#contactme").hide();
+  $("#socialmedia").show();
+  $(".circles").css("margin-top", "30px")
+});
+
+$(".circlefour").on("click", function (event) {
+    event.preventDefault();
+    console.log("hello");
+    $("#contactme").show();
+    $("#myworks").hide();
+    $("#about").hide();
+    $("#statement").hide();
+    $("#socialmedia").show();
+    $(".circles").css("margin-top", "30px")
+  });
